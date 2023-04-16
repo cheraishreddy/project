132646b247ec098c4b9b5b96e024ec371afe58c8
cheraishreddy <128718579+cheraishreddy@users.noreply.github.com>
2023-04-16 21:10:53 +0530

    asahdkhd

--- classification/views.py
+++ classification/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import FileSystemStorage
 import cv2
 import os
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
