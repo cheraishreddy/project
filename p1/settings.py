926fdd43ef3a0ed74abb807c657114df731d506a
cheraishreddy <128718579+cheraishreddy@users.noreply.github.com>
2023-04-16 14:37:20 +0530

    h1

--- p1/settings.py
+++ p1/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/stc/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
